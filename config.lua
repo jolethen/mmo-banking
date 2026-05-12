29e78b0b2da3e47c9781feb71be4ee2a111bdf95
jolethen <155651557+jolethen@users.noreply.github.com>
2026-05-12 17:30:28 +0530

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,12 @@
+mmo_banking = {}
+mmo_banking.config = {
+    item = "mingeld:geld",
+    unit_value = 100,
+    max_balance = 2000000000,
+    min_trans = 1,
+    tax_rate = 0.02,
+    server_fund = "__server_fund",
+    interest_rate = 0.001,
+    interest_interval = 604800, -- 7 days
+    cooldown = 1.5
+}
